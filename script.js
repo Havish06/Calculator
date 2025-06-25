8737a90cbc6f8b492c28b51eca5dff3d6dc02c2e
Havish Karthikeya Kanamarlapudi <havishkanamarlapudi@gmail.com>
2025-06-25 15:46:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let resultbox = document.getElementById("result-nums");
+let expression = "";
+
+document.querySelectorAll(".buttons").forEach(button => {
+    button.addEventListener("click", function() {
+        let value = button.innerText;
+
+        if (value === "=") {
+            expression = eval(expression);
+            resultbox.innerText = expression;
+        } 
+        else if (value === "C") {
+            expression = "";
+            resultbox.innerText = "";
+        }
+        else {
+            expression += value;
+            resultbox.innerText = expression;
+        }
+    });
+});
